14ff5288021721f3f45822b0fa5b999551b6ae7d
James Wilson <jbw@me.com>
2015-08-18 07:54:28 +0100

    Add script to configure sshd and regenerate host keys

---
+++ scripts/sshd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Skip DNS resolution
+sed -i 's|#UseDNS yes|UseDNS no|g' /etc/ssh/sshd_config
+sed -i 's|#GSSAPIAuthentication yes|GSSAPIAuthentication no|g' /etc/ssh/sshd_config
+sed -i '/GSSAPIAuthentication yes/d' /etc/ssh/sshd_config
+
+# Regenerate unique SSH host keys on first boot
+# https://www.digitalocean.com/company/blog/avoid-duplicate-ssh-host-keys/
+
+rm -f /etc/ssh/ssh_host_*
